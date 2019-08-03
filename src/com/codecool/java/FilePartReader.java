338460ae942ea9e13ccb295c4c68dfb82997cea3
Kruppa Tamás <krpp.tamas@gmail.com>
2019-08-03 20:11:37 +0200

    methods implemented  (palindrome left)

--- src/com/codecool/java/FilePartReader.java
+++ src/com/codecool/java/FilePartReader.java
@@ -1,5 +1,14 @@
 package com.codecool.java;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class FilePartReader {
 
     private String filePath;
@@ -12,17 +21,52 @@ public class FilePartReader {
 
     public void setup(String filePath, Integer fromLine, Integer toLine) {
 
+
         if (toLine < fromLine) throw new IllegalArgumentException("toLine is cannot be lower than fromLine");
         else if (fromLine < 1) throw new IllegalArgumentException("fromLine cannot be lower than 1");
 
     }
 
-    public String read(){
-        return filePath;
+    /** private static String readLineByLineJava8(String filePath)
+     {
+     StringBuilder contentBuilder = new StringBuilder();
+     try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
+     {
+     stream.forEach(s -> contentBuilder.append(s).append("\n"));
+     }
+     catch (IOException e)
+     {
+     e.printStackTrace();
+     }
+     return contentBuilder.toString();
+     }
+     **/
+
+    public String read(String filePath){
+        ArrayList<String> lines = new ArrayList<String>();
+        StringBuilder contentBuilder = new StringBuilder();
+        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s -> contentBuilder.append(s).append(" ujsor "));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return contentBuilder.toString();
     }
+
     //public String readLines() in the future this method will be this but before this i return void because without filling up this
     //it is not correct!!
-    public void readLines(){
+    public String readLines(Integer fromLine, Integer toLine){
+        StringBuilder contentBuilder = new StringBuilder();
+        String string = read("/home/kruppa/codecool/oop/5SI/filepartreader-testing-with-junit-kruppatomi/src/com/codecool/text/readtThis.txt");
+        List<String> list = new ArrayList<String>(Arrays.asList(string.split(" ujsor ")));
         //it will read the document from line to line with the metod read()
+        for(int i = fromLine; i<toLine;i++){
+            contentBuilder.append(list.get(i)).append(" ujsor ");
+        }
+        String newString = contentBuilder.toString();
+        return newString;
     }
 }
--- src/com/codecool/java/FileWordAnelyzer.java
+++ src/com/codecool/java/FileWordAnelyzer.java
@@ -2,30 +2,49 @@ package com.codecool.java;
 
 import com.codecool.java.FilePartReader;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class FileWordAnelyzer {
 
-    public FileWordAnelyzer(FilePartReader filePartReader) {
+    private FilePartReader filePartReader;
+
+    public FileWordAnelyzer() {
+        filePartReader = new FilePartReader();
 
     }
 
     public List getWordsOrderedAlphabetically (){
-        FilePartReader filePartReader = new FilePartReader();
-        filePartReader.readLines ();
+        String resorce = filePartReader.readLines (0,5);
+        List<String> list = new ArrayList<String>(Arrays.asList(resorce.split(" ujsor ")));
+        Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
+        return list;
         //returns the words ordered by alphabetically as an ArrayList
     }
 
     public List getWordsContainingSubstring (String subString ){
-        FilePartReader filePartReader = new FilePartReader();
-        filePartReader.readLines ();
+        String resorce = filePartReader.readLines (0,5);
+        List<String> newList = new ArrayList<>();
+        List<String> list = new ArrayList<String>(Arrays.asList(resorce.split(" ujsor ")));
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).contains("subString")){
+                newList.add(list.get(i));
+            }
+        }
+        return newList;
         //returns the words which contains the subString
     }
 
+/** PALINDROME!!!!!!!!!!!!!!! DON'T FORGET TO FILL THIS PART OUT!!
     public List getStringsWhichPalindromes (){
         FilePartReader filePartReader = new FilePartReader();
         filePartReader.readLines ();
         //returns the words from the String which are palindrome
     }
-
+**/
+public static void main(String[] args){
 }
+}
+
